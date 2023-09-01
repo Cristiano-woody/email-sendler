6ba2df69bbc35a0237850a367f036ef347e36789
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-09-01 16:50:55 -0300

    Refactor: adding return in Email Controller

--- src/main/java/com/msemail/emailsendler/controllers/EmailController.java
+++ src/main/java/com/msemail/emailsendler/controllers/EmailController.java
@@ -5,6 +5,7 @@ import com.msemail.emailsendler.model.EmailModel;
 import com.msemail.emailsendler.services.IEmailService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailController {
+    @Autowired
     IEmailService emailService;
 
     @PostMapping("/send-email")
@@ -20,8 +22,9 @@ public class EmailController {
         try {
             EmailModel email = new EmailModel();
             BeanUtils.copyProperties(emailDto, email);
-            emailService.execute(email);
+            this.emailService.execute(email);
 
+            return ResponseEntity.status(HttpStatus.CREATED).body("");
         } catch ( Error error) {
             ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
         }
